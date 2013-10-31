b68d4d9e01285567445ee216af8d6225c3604081
sgess <spencergessner@gmail.com>
2013-10-31 14:36:23 -0700

    edited gain adjustment tool

--- ES/set_gain.m
+++ ES/set_gain.m
@@ -1,6 +1,5 @@
-function gain = set_gain(omega,estimated_cost)
+function gain = set_gain(omega,estimated_cost,scale_factor)
 %set_gain: This function determines the gain based on the estimated cost
 
 w = mean(omega);
-ratio = 20;
-gain = w/(ratio*estimated_cost);
+gain = w/(scale_factor*estimated_cost);
