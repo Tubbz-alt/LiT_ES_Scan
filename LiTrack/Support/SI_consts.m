1ad53e495792fdc67d21357ba3e817ff7c33dd0a
Spencer Gessner <sgess@ad-pc92080.slac.stanford.edu>
2015-01-15 15:48:24 -0800

    added wavenumber

--- LiTrack/Support/SI_consts.m
+++ LiTrack/Support/SI_consts.m
@@ -7,5 +7,13 @@
  SI_re   = 2.81794092e-15;  % classical radius of electron [m]
  SI_eps0 = 8.854187817e-12; % permittivity of free space [F/m]
  SI_mu0  = 4e-7*pi;         % permeability of free space [T*m/A]
- SI_sband = 2856e6;         % SLAC cavity s-band frequency [Hz]
+ 
+ SI_sband  = 2856e6;          % SLAC s-band freq [Hz]
+ SI_sdegs  = 1/(360*SI_sband);  % SLAC s-band deg [s]
+ SI_sdegm  = SI_c*SI_sdegs;     % SLAC s-band deg [m]
  SI_sband_k = 2*pi*SI_sband/SI_c; % SLAC cavity s-band wavenumber [1/m]
+ 
+ SI_xband  = 4*SI_sband;        % SLAC x-band freq [Hz]
+ SI_xdegs  = SI_sdegs/4;        % SLAC x-band deg [s]
+ SI_xdegm  = SI_sdegm/4;        % SLAC x-band deg [s]
+ SI_xband_k = 2*pi*SI_xband/SI_c; % SLAC cavity s-band wavenumber [1/m]
